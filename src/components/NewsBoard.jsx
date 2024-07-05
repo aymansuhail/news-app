25bf93b00a436356f7d3764a5bb2e150d6534b74
aymansuhail <81426384+aymansuhail@users.noreply.github.com>
2024-07-05 10:28:43 +0530

    Update NewsBoard.jsx

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -4,14 +4,26 @@ import { NewsItem } from "./NewsItem";
 export const NewsBoard = ({ category, country }) => {
   const [articles, setArticles] = useState([]);
 
-  useEffect(() => {
-    const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${
-      import.meta.env.VITE_API_KEY
-    }`;
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => setArticles(data.articles));
-  }, [category, country]);
+ useEffect(() => {
+  const fetchNews = async () => {
+    try {
+      const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${
+        import.meta.env.VITE_API_KEY
+      }`;
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setArticles(data.articles);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      // Handle error state or notify the user appropriately
+    }
+  };
+
+  fetchNews();
+}, [category, country]);
 
   return (
     <div className="container">
